4003c1cc1a247bb5e4e4c4cc83150abc3f758f19
Toto <toto@kth.se>
2024-02-28 01:25:08 +0100

    refactor: Sorted and redid a few functions

--- src/Generator.hs
+++ src/Generator.hs
@@ -3,29 +3,38 @@ module Generator where
 
 import Data
 import Test.QuickCheck
-import Data.List(intercalate)
+import Data.List(intercalate, nub)
 import Data.Char(toUpper)
 
-name :: Gen String -> Gen String -> Gen String
-name (gfn) (gsn) = do
-  fn <- gfn
-  sn <- gsn
+
+
+--------------------------------------------------------------------------------
+-- | Attribute generators and helpers
+--------------------------------------------------------------------------------
+
+-- | generates a random full name
+name :: Gen String
+name = do
+  fn <- firstname
+  sn <- surname
   pure $ fn ++ " " ++ sn
 
+-- | Generates a full name using the given first and lastname
 name2 :: String -> String -> String
 name2 fn sn =  fn ++ " " ++ sn
 
-
-email :: Gen String -> Gen String -> Gen String
-email gfn gsn = do
-  fn <- gfn
-  sn <- gsn
+-- | Generates a random email
+email :: Gen String
+email = do
+  fn <- firstname
+  sn <- surname
   n <- num
   k <- num
   pure $ (take n fn)++(take k sn)++emaildomain
   where
     num = chooseInt (1,5)
 
+-- | Generates an email with the specified first and lastname
 email2 :: String -> String -> Gen String
 email2 fn sn = do
   n <- num
@@ -34,6 +43,7 @@ email2 fn sn = do
   where
     num = chooseInt (1,5)
 
+-- | Creates a date with the specified year
 date :: String -> Gen String
 date year = do
   month <- chooseInt (1,12)
@@ -47,7 +57,43 @@ date year = do
     (+-) :: String -> String -> String
     (+-) a b = a ++ "-" ++ b
 
-insertUser :: Gen String
+-- | Produces a list of unique primary keys at the specified length
+--   Obs cannot generate more than 88888 numbers
+primaryKeys :: Int -> Gen [Int]
+primaryKeys n = do
+  if n > 88888 then pure $ fail "Cannot generate more than upper bound 88 888 numbers" -- this just makes an empty list..
+    else loop $ numlist n
+  where
+    loop gl = do
+      l <- gl
+      let ls = nub l
+      if length ls < n then
+        loop $ increase (n - length ls) ls
+        else pure ls
+    increase i l = do
+      ns <- numlist i
+      pure $ l ++ (nub ns)
+    numlist n = vectorOf n (chooseInt (11111,99999)) -- 88 888 unique numbers
+
+
+
+--------------------------------------------------------------------------------
+-- | Statement generators
+--------------------------------------------------------------------------------
+type InsertStatement = String
+type SchemaName = String
+type Atributes = String
+type Values = String
+
+-- | Make an n long list of randomly generated items
+gen :: Int -> Gen a -> Gen [a]
+gen n g = do
+  a <- g
+  b <- gen (n - 1) g
+  pure (a : b)
+
+
+insertUser :: Gen InsertStatement
 insertUser = do
   fn <- firstname
   sn <- surname
@@ -56,14 +102,24 @@ insertUser = do
   date <- date "2024"
   pure $ insertStatement "user" ["name","email","joinDate"] [name,email,date]
 
+insertUsers :: Int -> Gen [InsertStatement]
+insertUsers n = do
+  primarykeys <- primaryKeys n
+  firstnames <- gen n firstname
+  lastnames <- gen n surname
+  pure $ map makeUser (zip3 primarykeys firstnames lastnames)
+  where
+    makeUser (pm,fn,sn) =
+      insertStatement "user" ["userId","name"] [show pm,name2 fn sn]
 
-type SchemaName = String
-type Atributes = String
-type Values = String
-insertStatement :: String -> [Atributes] -> [Values] -> String
+
+insertStatement :: SchemaName -> [Atributes] -> [Values] -> InsertStatement
 insertStatement schemaName as vs =
   "INSERT INTO "++(map toUpper schemaName)
   ++ listify as ++ "\n"
   ++ "VALUES " ++ listify vs ++ ";\n"
   where
     listify as = "("++ intercalate "," as ++ ")"
+
+test n = do a <-generate $  insertUsers n; mapM_ (putStrLn) a
+test2 = do a<- generate $ insertUser; putStrLn a
