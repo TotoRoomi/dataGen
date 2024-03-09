b26fda2fee74a2e5c0fb704857146014004a81e1
Toto <toto@kth.se>
2024-03-09 21:00:02 +0100

    tests update

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,31 @@
+import Test.QuickCheck
+import Populator
+import Generator
+import Pretty
+import Example
+import Data.List(notElem)
+
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+
+pairTest = do
+  l <- generate $ primaryKeys 100
+  list <- generate $ pairs (1,10) l
+  mapM_ (\(b,c) -> putStrLn$ (showPSQLTYPE b) ++ ","++(showPSQLTYPE c))   list
+
+pairTest2 = do
+  l <- generate $ primaryKeys 100
+  list <- generate $ pairs (10,100) l
+  putStrLn . show $ length list
+
+nonReflexivityProp :: Gen Bool
+nonReflexivityProp = do
+  l <- primaryKeys 100
+  list <- pairs (10,100) l
+  pure $ (length list) == (length $ filter (\(a,b) -> (b,a) `notElem` list) list)
+
+testFriend = do
+  ks <- generate $ primaryKeys 5
+  pretty $ friend ks
